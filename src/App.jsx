b700a6038947d35e8509f3ede8b822ea1740bce9
wineboutique2023-glitch <wineboutique2023@gmail.com>
2026-06-08 15:25:26 +1000

    Update App.jsx

--- src/App.jsx
+++ src/App.jsx
@@ -1242,7 +1242,7 @@ created_at: checkoutForm.checkout_datetime
 
                 <label>Service Date / Time</label>
 <input
-  type="datetime-local"
+  type="date"
   value={checkoutForm.checkout_datetime || ""}
   onChange={(e) =>
     setCheckoutForm({
